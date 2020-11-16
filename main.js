8a8e66854ae7c7c4c3fe2216442598eb4a6cb26b
gptinlin <garrett.tinlin@gmail.com>
2020-11-15 20:57:25 -0800

    fourth commit - created js to being images to html

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-
 $( document ).ready(function() {
     console.log( "ready!" );
-    $('#fetch-from-api').on('click', () => {
-        console.log('I was clicked');
-    $.get('api.giphy.com/v1/gifs/search?api-key=bu2jBN3FbA4qjHRnUR5hglMKejHnNNCR',
-        function(data) {
-            console.log(date);
-        //$( ".result" ).html( data );
-        //alert( "Load was performed." );
+
+    $('#fetch-from-api').on('click', async () => {
+      console.log('I was clicked!');
+      $.get('https://api.giphy.com/v1/gifs/search?api_key=4zv45wmD5vJvLn2UE3B4tNBY1ny9kZZV&q=smores&limit=10', function(giphyResponse) {
+        console.log(giphyResponse);
+        console.log('giphyData', giphyResponse.data)
+        console.log('giphyData result 0 (1st result)', giphyResponse.data[0]);
+        console.log('giphyData result 0 (1st result) - all images list', giphyResponse.data[0].images);
+        console.log('giphyData result 0 (1st result) - all images list - original image', giphyResponse.data[0].images.original);
+
+        console.log('giphyData result 0 (1st result) - all images list - original image - url', giphyResponse.data[0].images.original.url);
+
+
+        for (let image of giphyResponse.data) {        
+        let imgHtml = `<img src=${image.images.original.url} />`
+        $('#first-result-image').append(imgHtml);
+        }
       });
+
     })
+
+
+
 });
