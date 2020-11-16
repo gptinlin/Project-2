bcdca62214cdf7dfc6d632728e12cb2a336f0042
gptinlin <garrett.tinlin@gmail.com>
2020-11-16 13:30:03 -0800

    6th commit - used zoom js to help with search bar

--- main.js
+++ main.js
@@ -1,34 +1,69 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
+    // $('#search-field').on('change', () => {
+    //   console.log('input was changed!');
+    // });
+
+    // $('#search-field').change(() => {
+    //   console.log('input was changed!');
+    // });
+
+
+
+
+
     $('#search-button').on('click', () => {
-        // console.log('I was clicked');
-        // console.log($('#search-field').val());
-        let searchTerm = $('#search-field').val();
-        console.log(searchTerm);
-        
-    })
+      // console.log('i was clicked');
+      // console.log($('#search-field').val());
+      let searchTerm = $('#search-field').val();
 
-      $.get('https://api.giphy.com/v1/gifs/search?api_key=4zv45wmD5vJvLn2UE3B4tNBY1ny9kZZV&q=${searchTerm}', function(giphyResponse) {
-        //console.log(giphyResponse);
-        //console.log('giphyData', giphyResponse.data)
-        //console.log('giphyData result 0 (1st result)', giphyResponse.data[0]);
-        //console.log('giphyData result 0 (1st result) - all images list', giphyResponse.data[0].images);
-        //console.log('giphyData result 0 (1st result) - all images list - original image', giphyResponse.data[0].images.original);
+      console.log(searchTerm);
 
-        //console.log('giphyData result 0 (1st result) - all images list - original image - url', giphyResponse.data[0].images.original.url);
+      let url = "https://api.giphy.com/v1/gifs/search?api_key=4zv45wmD5vJvLn2UE3B4tNBY1ny9kZZV&q=" + searchTerm;
 
-        let resultHtml = '';
-        for (let image of giphyResponse.data) {     
-        let imgHtml = `<img src=${image.images.original.url} />`
-        let resultsHtml = resultsHtml + imgHtml;
+
+      //jquery way of doin it
+      $.get(url, function(giphyResponse) {
+        // console.log(giphyResponse);
+        // console.log('giphyData', giphyResponse.data)
+        // console.log('giphyData result 0 (1st result)', giphyResponse.data[0]);
+        // console.log('giphyData result 0 (1st result) - all images list', giphyResponse.data[0].images);
+        // console.log('giphyData result 0 (1st result) - all images list - original image', giphyResponse.data[0].images.original);
+
+        // console.log('giphyData result 0 (1st result) - all images list - original image - url', giphyResponse.data[0].images.original.url);
+// 
+
+        let resultsHtml = '';
+
+
+        // for(let image of response.images){
+        //   resultsHtml = resultsHtml + `<img src=${image.images.original.url} />`
+        // }
+
+        // $('#results').thml(resultsHtml)
+
+
+        for (let image of giphyResponse.data) {                
+          // let imgHtml = `<img src=${image.images.original.url} class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 col-span-12 sm:col-span-6 md:col-span-4 "/>`
+          let imgHtml = `<img src=${image.images.original.url} />`
+          resultsHtml = resultsHtml + imgHtml;          
         }
+
         console.log(resultsHtml);
-        $('#search-result').html(imgHtml);
-        
+
+        $('#search-results').html(resultsHtml);
+
+
       });
 
-    
-    
+
+
+
+
+    })
+
+
 
 
 });
